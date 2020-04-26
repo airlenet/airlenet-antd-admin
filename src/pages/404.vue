b1f5b97cec251967b7d4144c71ea74216defc4fb
airlenet <airlenet@163.com>
2020-04-26 13:41:49 +0800

    404页面

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,9 +1,18 @@
 <template>
-  <div></div>
+  <a-result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+  >
+
+    <a-button slot="extra" type="primary" @click="() => $router.push('/')">
+    Back Home
+    </a-button>
+  </a-result>
 </template>
 
 <script>
 export default {
-  name: "404"
+  name: "NoFoundPage"
 };
 </script>
